b0e303cd2c7b96d882fe76e0322b2fd2d781b6e1
streetyglobaladmin <rareonearthkhaja@gmail.com>
2026-08-10 10:18:18 -0400

    fix(android): pin compileSdk=36, disable Gradle daemon, reduce JVM heap for release APK build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.iqracompanion.edu"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
